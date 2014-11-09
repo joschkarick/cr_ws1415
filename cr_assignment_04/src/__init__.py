7d67f2f3020a16cbc099ad44982b607cfbc164ba
Joschka Rick <joschkarick+git@gmail.com>
2014-11-09 17:49:18 +0100

    Added image loading via PIL

--- cr_assignment_04/src/__init__.py
+++ cr_assignment_04/src/__init__.py
@@ -1,2 +1,42 @@
 __author__ = 'Fernando Morillo, Joschka Rick'
 
+from PIL import Image
+
+
+IMAGE_PATH = '../Assignment_4_Grid_Map.png'
+LASER_RANGE = 1500
+PIXEL_RESOLUTION = 4
+OPENING_ANGLE = 270
+ANGULAR_RESOLUTION = 2
+
+
+def read_png(png_file_path):
+    img = Image.open(png_file_path)
+    raw_data = img.tostring()
+
+    data = [[0 for y in range(img.size[1])] for x in range(img.size[0])]
+    for x in range(img.size[0]):
+        for y in range(img.size[1]):
+            try:
+                data[x][y] = raw_data[x * img.size[0] + y]
+            except IndexError as e:
+                print("Error while writing index=", (x * img.size[0] + y), "to [x=", x, ", y=", y, "]")
+
+    return data, img.size
+
+
+def trace_ray(origin, destination, data):
+    pass
+
+
+def get_laserscan(pose):
+    pass
+
+
+def main():
+    data, size = read_png(IMAGE_PATH)
+    pass
+
+
+if __name__ == "__main__":
+    main()
